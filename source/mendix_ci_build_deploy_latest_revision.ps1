77d9e2dbee6777ec3979caed3ce8f3a03e96f348
Bart Poelmans <bart.poelmans@bizzomate.com>
2019-10-18 20:10:10 +0200

    Made fixes and changes
    Fixed header part and testen script with deploy enabled.

--- source/mendix_ci_build_deploy_latest_revision.ps1
+++ source/mendix_ci_build_deploy_latest_revision.ps1
@@ -41,7 +41,7 @@ if (!$environmentconfig) {
 $url = 'https://deploy.mendix.com/api/1/'
 
 # IMPORT MENDIX API CREDENTIALS
-$headers = Import-PowerShellDataFile $apicredentials
+$headers = Import-PowerShellDataFile $apiheaders
 
 # IMPORT ENVIRONMENT CONFIGURATION 
 $configuration = Import-PowerShellDataFile $environmentconfig
@@ -53,27 +53,30 @@ $branchName = $configuration.Environment.BranchName
 
 $branch = Get-Branch $headers $url $appName $branchName
 
-"Branch to build: $branch"
+Write-Host "Branch to build: $branch"
 $latestBuiltRevision = $branch.LatestTaggedVersion.Substring($branch.LatestTaggedVersion.LastIndexOf('.') + 1)
 $latestRevisionNumber = $branch.LatestRevisionNumber
 
 if ($latestBuiltRevision -eq $latestRevisionNumber) {
-    "It is not needed to build, as the latest revision is already built."
+    Write-Host "It is not needed to build, as the latest revision is already built."
     exit
-}
-
+}   
+    
 $versionWithoutRevision = $branch.LatestTaggedVersion.Remove($branch.LatestTaggedVersion.LastIndexOf('.'))
 $packageId = Start-Build $headers $url $appName $branchName $latestRevisionNumber $versionWithoutRevision
 $built = Wait-For-Built $headers $url $appName $packageId 600
 
 if($built -eq $false) {
-    "No build succeeded within 10 minutes."
+    Write-Host "No build succeeded within 10 minutes."
     exit
 }
 
 if ($dodeploy) {
+    
+    # TODO Check if latest built is already deployed on environment.
+    
+    
     Stop-App $headers $url $appName $environment
-    # Clear-App $headers $url $appName $environment
     Move-Package $headers $url $appName $environment $packageId
     # TODO Set config when requested
     $startJobId = Start-App $headers $url $appName $environment
